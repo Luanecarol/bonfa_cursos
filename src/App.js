931c2574b4d7870978918651dda0ab3f9e29eb8e
Luane Caroline <luanecrln@gmail.com>
2022-05-23 20:08:30 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Center">
+    <div className="App">
+      <div className="menu">
+         <div className='logo'>
+           <h3>BONFA</h3>
+         </div>
+         <div className='item-menu'>
+           <a className='item-1' href="#">Início</a>
+           <a className='item-1' href="#">Plano</a>
+           <a className='item-1' href="#">Contato</a>
+           
+
+         </div>
+       </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
