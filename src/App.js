00b77b77b80c7a5b821b8ec38ade315e11acfebf
Luane Caroline <luanecrln@gmail.com>
2022-05-25 21:38:41 -0300

    tela login ok

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Center">
-    <div className="App">
-      <div className="menu">
-         <div className='logo'>
-           <h3>BONFA</h3>
-         </div>
-         <div className='item-menu'>
-           <a className='item-1' href="#">Início</a>
-           <a className='item-1' href="#">Plano</a>
-           <a className='item-1' href="#">Contato</a>
-           
+    <div className='container'>
+      <div className='container-login'>
+        
+          <form className='login-form'>
+            <span className='login-form-title-1'>ead<h3 className='login-form-title-2'>fy.</h3></span>
+            <span className='login-form-title-3'>Monte seu negócio digital sem precisar produzir um vídeo sequer</span>
 
-         </div>
-       </div>
-    </div>
-    </div>
+<div className='wrap-input'>
+  <input className='input-1' placeholder='Seu Login' type='email'/>
+  <input className='input-2' placeholder='Seu Email' type='email'/>
+  <div className='checkbox-senha'>
+  <input className='checkbox' type='checkbox'/> 
+    <span>Conectar automaticamente</span>
+    <a href="#">Esqueci a Senha </a>
+  </div>
+  <button className='btn-entrar'>Entrar</button>
+  <h3 className='text'>Ou</h3>
+  <h3 className='text-2'>  <a href='#'>Fazer Cadastro</a> </h3>
+
+
+</div>
+
+          </form>
+        </div>
+      </div>
+    
   );
 }
 
