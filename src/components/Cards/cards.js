eaf942f03add0ced8751d7bc54dbafc8176aaa22
betolarbac <betocr.dev@gmail.com>
2022-06-22 20:40:27 -0300

    teste

--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -1,95 +1,97 @@
 import './cards.css';
 import React from 'react';
+import { Sidebar } from '../Sidebar/sidebar';
 
-function Cards(){
+export function Cards(){
 return(
-    <div className='container'>
-        <span className='tittle-container'>Área de membros</span>
-        <span className='container-add'>+ Criar Nova Área</span>
-        <span className='container-subtittle'>Suas Áreas De Membros</span>
 
-       <div className='container-card'>
+  <><Sidebar /><div className='container'>
+    <span className='tittle-container'>Área de membros</span>
+    <span className='container-add'>+ Criar Nova Área</span>
+    <span className='container-subtittle'>Suas Áreas De Membros</span>
 
-        <div className='card'>
-            <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
-            <span className='text-card'>ADschool Prime</span>
-            <span ><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
-            <span ><i className="fa-solid fa-user-graduate icon-edit"></i></span>
+    <div className='container-card'>
 
-            <div className='container-icon'>
-                <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
-                <span className='icon-student-text'>354</span>
-                <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
-                <span className='icon-hand-text'>4k</span>
+      <div className='card'>
+        <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
+        <span className='text-card'>ADschool Prime</span>
+        <span><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
+        <span><i className="fa-solid fa-user-graduate icon-edit"></i></span>
 
-                <div >
-                <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
-                </div>
-           </div>
+        <div className='container-icon'>
+          <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
+          <span className='icon-student-text'>354</span>
+          <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
+          <span className='icon-hand-text'>4k</span>
 
-         </div>
+          <div>
+            <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
+          </div>
+        </div>
 
-         <div className='card'>
-            <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
-            <span className='text-card'>ADschool Prime</span>
-            <span ><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
-            <span ><i className="fa-solid fa-user-graduate icon-edit"></i></span>
+      </div>
 
-            <div className='container-icon'>
-                <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
-                <span className='icon-student-text'>354</span>
-                <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
-                <span className='icon-hand-text'>4k</span>
+      <div className='card'>
+        <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
+        <span className='text-card'>ADschool Prime</span>
+        <span><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
+        <span><i className="fa-solid fa-user-graduate icon-edit"></i></span>
 
-                <div >
-                <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
-                </div>
-           </div>
+        <div className='container-icon'>
+          <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
+          <span className='icon-student-text'>354</span>
+          <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
+          <span className='icon-hand-text'>4k</span>
 
-         </div>
+          <div>
+            <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
+          </div>
+        </div>
 
-         <div className='card'>
-            <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
-            <span className='text-card'>ADschool Prime</span>
-            <span ><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
-            <span ><i className="fa-solid fa-user-graduate icon-edit"></i></span>
+      </div>
 
-            <div className='container-icon'>
-                <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
-                <span className='icon-student-text'>354</span>
-                <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
-                <span className='icon-hand-text'>4k</span>
+      <div className='card'>
+        <div className='card-image'><i className="fa-solid fa-camera fa-1x icon-image"></i></div>
+        <span className='text-card'>ADschool Prime</span>
+        <span><i className="fa-solid fa-user-graduate icon-arrow"></i></span>
+        <span><i className="fa-solid fa-user-graduate icon-edit"></i></span>
 
-                <div >
-                <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
-                <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
-                </div>
-           </div>
+        <div className='container-icon'>
+          <span> <i className="fa-solid fa-user-graduate icon-student"></i></span>
+          <span className='icon-student-text'>354</span>
+          <span> <i className="fa-solid fa-user-graduate icon-hand"></i></span>
+          <span className='icon-hand-text'>4k</span>
 
-         </div>
+          <div>
+            <span> <i className="fa-solid fa-user-graduate icon-anexo"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-copy"></i></span>
+            <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
+          </div>
+        </div>
 
-
-       </div>
-
-
-   <div className='suport'>       
-     <span className='suport-text'>Suporte <br/> Estamos Online</span>
-     <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
-     <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
-   
-   </div>
-
-
-
-
+      </div>
 
 
     </div>
+
+
+    <div className='suport'>
+      <span className='suport-text'>Suporte <br /> Estamos Online</span>
+      <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
+      <span> <i className="fa-solid fa-user-graduate icon-delete"></i></span>
+
+    </div>
+
+
+
+
+
+
+  </div></>
 
 
 
@@ -98,4 +100,3 @@ return(
 
 
 
-export default Cards;
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -9,13 +9,14 @@ import { Pay } from "../components/Integracoes/Pay";
 import { Planos } from "../components/Planos/planos";
 import { Sidebar } from "../components/Sidebar/sidebar";
 import { Dashboard } from "../components/dashboard/dashboard";
+import {Cards } from '../components/Cards/cards'
 
 
 export function Rotas(){
     return(
         <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Cards />} />
           <Route path="dashboard" element={<Dashboard />} />
 
           <Route path="Planos" element={<Planos />} />
